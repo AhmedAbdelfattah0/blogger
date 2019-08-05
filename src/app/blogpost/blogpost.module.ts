3e043949f89ca51181466daa32fe5460e5b84a55
Abdelfatah <ahmed.abdelfatah@arabyads.com>
2019-08-05 15:01:54 +0200

    Creating modules

---
+++ src/app/blogpost/blogpost.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BlogpostRoutingModule } from './blogpost-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BlogpostRoutingModule
+  ],
+  declarations: []
+})
+export class BlogpostModule { }
---
+++ src/app/cmspage/cmspage.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CmspageRoutingModule } from './cmspage-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CmspageRoutingModule
+  ],
+  declarations: []
+})
+export class CmspageModule { }
