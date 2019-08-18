e2c56068489fc2646319af5afdc6365a76da2c27
Abdelfatah <ahmed.abdelfatah@arabyads.com>
2019-08-18 13:49:15 +0200

    genrate admin-dashboard,admin,manage-blogs, manage-categories, manage-pages, login components and auth service

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AdminComponent } from './admin/admin.component';
+import { ManageBlogsComponent } from './manage-blogs/manage-blogs.component';
+import { ManageCategoriesComponent } from './manage-categories/manage-categories.component';
+import { ManagePagesComponent } from './manage-pages/manage-pages.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AdminRoutingModule
   ],
-  declarations: []
+  declarations: [AdminDashboardComponent, AdminComponent, ManageBlogsComponent, ManageCategoriesComponent, ManagePagesComponent]
 })
 export class AdminModule { }
