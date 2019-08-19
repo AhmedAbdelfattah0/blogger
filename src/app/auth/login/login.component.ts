90be1463858713305a1f90fa9be5047f9c6ba1af
Abdelfatah <ahmed.abdelfatah@arabyads.com>
2019-08-19 11:16:40 +0200

    Import AuthService, FormBuilder, Validators, FormGroup, Router into login.component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  submitted = false;
+  returnUrl: string;
+  error: {};
+  loginError: string;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+    ) { }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    this.authService.logout();
   }
 
+  get username() { return this.loginForm.get('username'); }
+  get password() { return this.loginForm.get('password'); }
+
+  onSubmit() {
+    this.submitted = true;
+    this.authService.login(this.username.value, this.password.value).subscribe((data) => {
+       if (this.authService.isLoggedIn) {
+          const redirect = this.authService.redirectUrl ? this.authService.redirectUrl : '/admin';
+          this.router.navigate([redirect]);
+        } else {
+          this.loginError = 'Username or password is incorrect.';
+        }
+      },
+      error => this.error = error
+    );
+  }
 }
